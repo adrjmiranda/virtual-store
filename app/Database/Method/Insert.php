b7d75cf7f1cb4b6435ff9a6c5931c2f8d6a913ae
Adriano Miranda <adrjmiranda@gmail.com>
2025-11-27 21:15:30 -0300

    chore: created insert trait to query builder

--- app/Database/Method/Insert.php
+++ app/Database/Method/Insert.php
@@ -2,12 +2,35 @@
 
 namespace App\Database\Method;
 
+use InvalidArgumentException;
+use LogicException;
+
 trait Insert
 {
+  private array $dataToInsert = [];
+  private ?string $insertColumns = null;
+  private ?string $insertValues = null;
+
   public function insert(array $data): static
   {
-    // TODO:
-    // ...
+    if (empty($data)) {
+      throw new InvalidArgumentException("The data submitted for update must not be empty.", 500);
+    }
+
+    $this->dataToInsert = $data;
+    $columns = array_keys($data);
+    $this->insertColumns = '(' . implode(', ', $columns) . ')';
+    $this->insertValues = '(:' . implode(', :', $columns) . ')';
+
     return $this;
+  }
+
+  private function getInsert(): ?string
+  {
+    if (!$this->table) {
+      throw new LogicException("No table defined. Use ->from('table') before building the query.", 500);
+    }
+
+    return $this->insertColumns === null || $this->insertValues === null ? null : "INSERT INTO {$this->table} {$this->insertColumns} VALUES {$this->insertValues} RETURNING id";
   }
 }
