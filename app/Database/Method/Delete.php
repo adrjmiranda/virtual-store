e9047cf060d76bd95760b6f669741e6115088958
Adriano Miranda <adrjmiranda@gmail.com>
2025-11-27 21:20:04 -0300

    chore: created delete trait to query builder

--- app/Database/Method/Delete.php
+++ app/Database/Method/Delete.php
@@ -2,12 +2,25 @@
 
 namespace App\Database\Method;
 
+use LogicException;
+
 trait Delete
 {
+  private ?string $delete = null;
+
   public function delete(): static
   {
-    // TODO:
-    // ...
+    if (!$this->table) {
+      throw new LogicException("No table defined. Use ->from('table') before building the query.", 500);
+    }
+
+    $this->delete = "DELETE FROM {$this->table}";
+
     return $this;
+  }
+
+  private function getDelete(): ?string
+  {
+    return $this->delete ?? null;
   }
 }
