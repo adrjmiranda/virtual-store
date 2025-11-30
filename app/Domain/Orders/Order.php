8253c292af6b6b8e296f56a93503d4e45e75d258
Adriano Miranda <adrjmiranda@gmail.com>
2025-11-30 07:16:38 -0300

    chore: created Order entity class

---
+++ app/Domain/Orders/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Domain\Orders;
+
+use App\Domain\ValueObjects\CreatedAt;
+use App\Domain\ValueObjects\Id;
+use App\Domain\ValueObjects\Shared\DatabaseEntity;
+use App\Domain\ValueObjects\UpdatedAt;
+use App\Domain\ValueObjects\UserId;
+
+class Order extends DatabaseEntity
+{
+  public function __construct(
+    private ?Id $id,
+    private UserId $userId,
+    private Status $status,
+    private Total $total,
+    private ?ShippingAddress $shippingAddress,
+    private ?CreatedAt $createdAt,
+    private ?UpdatedAt $updatedAt
+  ) {
+  }
+}
