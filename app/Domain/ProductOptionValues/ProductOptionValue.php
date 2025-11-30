23cdb591c07449b75ad06d8f0fe6bdffd397a515
Adriano Miranda <adrjmiranda@gmail.com>
2025-11-30 07:24:54 -0300

    chore: created ProductOptionValue entity class

---
+++ app/Domain/ProductOptionValues/ProductOptionValue.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Domain\ProductOptionValues;
+
+use App\Domain\ValueObjects\CreatedAt;
+use App\Domain\ValueObjects\Id;
+use App\Domain\ValueObjects\OptionId;
+use App\Domain\ValueObjects\Shared\DatabaseEntity;
+use App\Domain\ValueObjects\UpdatedAt;
+
+class ProductOptionValue extends DatabaseEntity
+{
+  public function __construct(
+    private ?Id $id,
+    private OptionId $optionId,
+    private Value $value,
+    private ?CreatedAt $createdAt,
+    private ?UpdatedAt $updatedAt
+  ) {
+  }
+}
