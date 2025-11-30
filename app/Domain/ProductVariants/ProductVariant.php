266b8d7437c0964ea6b1a416921d5657964da362
Adriano Miranda <adrjmiranda@gmail.com>
2025-11-30 07:32:28 -0300

    chore: created ProductVariant entity class

---
+++ app/Domain/ProductVariants/ProductVariant.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Domain\ProductVariants;
+
+use App\Domain\ValueObjects\CreatedAt;
+use App\Domain\ValueObjects\Id;
+use App\Domain\ValueObjects\IsActive;
+use App\Domain\ValueObjects\Price;
+use App\Domain\ValueObjects\ProductId;
+use App\Domain\ValueObjects\Shared\DatabaseEntity;
+use App\Domain\ValueObjects\UpdatedAt;
+
+class ProductVariant extends DatabaseEntity
+{
+  public function __construct(
+    private ?Id $id,
+    private ProductId $productId,
+    private Sku $sku,
+    private Price $price,
+    private Stock $stock,
+    private IsActive $isActive,
+    private ?CreatedAt $createdAt,
+    private ?UpdatedAt $updatedAt
+  ) {
+  }
+}
