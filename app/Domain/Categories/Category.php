dad52e7c0d7c67e4991a018f90612808ffc11df0
Adriano Miranda <adrjmiranda@gmail.com>
2025-11-29 22:39:14 -0300

    chore: created Category entity class

---
+++ app/Domain/Categories/Category.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Domain\Categories;
+
+use App\Domain\ValueObjects\CreatedAt;
+use App\Domain\ValueObjects\Description;
+use App\Domain\ValueObjects\Id;
+use App\Domain\ValueObjects\IsActive;
+use App\Domain\ValueObjects\Name;
+use App\Domain\ValueObjects\Shared\DatabaseEntity;
+use App\Domain\ValueObjects\Slug;
+use App\Domain\ValueObjects\UpdatedAt;
+
+class Category extends DatabaseEntity
+{
+  public function __construct(
+    private ?Id $id,
+    private Name $name,
+    private Slug $slug,
+    private ?Description $description,
+    private ?ParentId $parentId,
+    private IsActive $isActive,
+    private ?CreatedAt $createdAt,
+    private ?UpdatedAt $updatedAt
+  ) {
+  }
+}
